ff465ba2b44bc8146e5c61144380f754d4644a5b
Andre Lucio <andre.lucio@insidesistemas.com.br>
2022-03-30 13:20:45 -0300

    Criado novo teste que insere e deleta

--- cypress/integration/services/Books/testes/DELETEBooks.spec.js
+++ cypress/integration/services/Books/testes/DELETEBooks.spec.js
@@ -1,9 +1,10 @@
 import * as DELETEBooks from '../requests/DELETEBooks.request'
-
-//obtem os books antes de deletar:
 import * as GETBooks from '../requests/GETBooks.request'
+import * as POSTBooks from '../requests/POSTBooks.request'
 
 describe('DELETE Books', () => {
+
+    //----------------------------------------------------
     it('Deletar um livro', () => {
         //Obtem os livros antes de deletar:
         GETBooks.allBooks().then((resAllBooks) => {
@@ -13,6 +14,23 @@ describe('DELETE Books', () => {
             DELETEBooks.deleteBook(resAllBooks.body[0].id).should((resDeleteBooks) => {
                 //Espera uma resposta 200 de sucesso:
                 expect(resDeleteBooks.status).to.equal(200);
+            })
+        })
+
+    });
+
+    //----------------------------------------------------
+    it.only('Cria e exlui o Livro', () => {
+
+        //cria o livro:
+        POSTBooks.addBook().then((resAddBokk) => {
+
+            cy.log('Id:' + resAddBokk.body.id)
+            cy.log('Titulo:' + resAddBokk.body.title)
+
+            //Deleta o livro criado, pelo ID retornado:
+            DELETEBooks.deleteBook(resAddBokk.body.id).should((resDeleteBook) => {
+                expect(resDeleteBook.status).to.equal(200);
             })
         })
 
